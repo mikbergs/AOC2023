7d33a2ca2b383c780618242fd3ec9bbfbc4e2a05
mikael <mikael@bergstrom.it>
2023-12-03 17:39:56 +0100

    Solves day 1

---
+++ 1/main.py
@@ -0,0 +1,19 @@
+def process_line(line):    
+    integers = []
+
+    integers.append(get_first_integer(line))    
+    integers.append(get_first_integer(reversed(line)))    
+    return (integers[0]*10) + integers[1]
+
+def get_first_integer(elements):
+    for element in elements:
+        if element.isdigit():
+            return (int(element))        
+
+with open('input', 'r') as file:    
+    integers = []    
+    for line in file:        
+        integers.append(process_line(line))
+    print(sum(integers))
+        
+    
